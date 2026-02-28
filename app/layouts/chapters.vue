bebc7d998dc5e27043c3a9c82a3b96d67dda8525
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2026-02-28 01:18:38 +0100

    feat: Tree Color mode switch

--- app/layouts/chapters.vue
+++ app/layouts/chapters.vue
@@ -1,6 +1,41 @@
 <script setup lang="ts">
 import type { ContentNavigationItem } from '@nuxt/content'
 import { de } from '@nuxt/ui/locale'
+import type { TreeItem } from '@nuxt/ui'
+
+const colorMode = useColorMode()
+
+const items = ref<TreeItem[]>([
+  {
+    id: 'system',
+    label: de.messages.colorMode.system,
+    icon: 'i-lucide-monitor'
+  },
+  {
+    id: 'light',
+    label: de.messages.colorMode.light,
+    icon: 'i-lucide-sun'
+  },
+  {
+    id: 'dark',
+    label: de.messages.colorMode.dark,
+    icon: 'i-lucide-moon'
+  }
+])
+
+const colorModeSelection = computed({
+  get: () => {
+    switch (colorMode.preference) {
+      case 'light':
+        return items.value[1]
+      case 'dark':
+        return items.value[2]
+      default:
+        return items.value[0]
+    }
+  },
+  set: value => colorMode.preference = value?.id
+})
 
 const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
 </script>
@@ -26,7 +61,11 @@ const navigation = inject<Ref<ContentNavigationItem[]>>('navigation')
             <div class="text-sm font-semibold flex items-center gap-1.5">
               {{ de.messages.contentSearch.theme }}
             </div>
-            <UColorModeSelect />
+            <UTree
+              v-model="colorModeSelection"
+              :items="items"
+              color="neutral"
+            />
           </div>
         </UPageAside>
       </template>
