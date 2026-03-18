245abddad1314a3b9b0a527c379d03aeb2902491
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2026-03-18 03:05:25 +0100

    feat(audio): dB volume.

--- app/plugins/audio.client.ts
+++ app/plugins/audio.client.ts
@@ -1,9 +1,12 @@
 export default defineNuxtPlugin(() => {
   const audioContext = new AudioContext()
 
-  const masterGainSliderValue = ref(1.0)
+  const MIN_DB = -40
+  const sliderToGain = (v: number) => v <= 0 ? 0 : 10 ** (MIN_DB * (1 - v) / 20)
+
+  const masterGainSliderValue = ref(1)
   const masterGainNode = new GainNode(audioContext, {
-    gain: masterGainSliderValue.value
+    gain: sliderToGain(masterGainSliderValue.value)
   })
 
   const isAudioEnabled = ref(false)
@@ -15,12 +18,12 @@ export default defineNuxtPlugin(() => {
   masterGainNode.connect(muteNode)
   muteNode.connect(audioContext.destination)
 
-  watch(masterGainSliderValue, (newValue: number) => {
-    masterGainNode.gain.setTargetAtTime(newValue, 0, 0.02)
+  watch(masterGainSliderValue, (v: number) => {
+    masterGainNode.gain.setTargetAtTime(sliderToGain(v), 0, 0.02)
   })
 
   watch(isAudioEnabled, (value) => {
-    muteNode.gain.setTargetAtTime(value ? 1 : 0, 0, 0.3)
+    muteNode.gain.setTargetAtTime(value ? 1 : 0, 0, 0.05)
   })
 
   const isAudioRenderingEnabled = useState<boolean>(
